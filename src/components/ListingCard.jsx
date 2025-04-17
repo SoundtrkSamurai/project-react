8c7d568aad95e159e2fd265b1c49ab496c70a9db
SoundtrkSamurai <furtimat3@yahoo.com>
2025-04-17 15:56:26 -0700

    data-fetching: update listing card with more info

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,7 +1,11 @@
+import { DollarSign, Pin, Users } from 'lucide-react';
+
 import { Card, CardContent } from '@/components/ui';
 import { getImageUrl } from '@/lib/utils/images';
 
-const ListingCard = ({ listing: { name, images } }) => {
+const ListingCard = ({
+  listing: { name, images, price, location, maxGuests },
+}) => {
   return (
     <Card className='w-[320px]'>
       <img
@@ -9,8 +13,22 @@ const ListingCard = ({ listing: { name, images } }) => {
         src={getImageUrl(images[0])}
         alt={name}
       />
-      <CardContent className='p-4'>
-        <h2 className='mb-0 text-xl font-semibold'>{name}</h2>
+      <CardContent className='flex flex-col gap-2 p-4'>
+        <h2 className='mb-2 text-xl font-semibold'>{name}</h2>
+        <div className='flex items-center gap-2'>
+          <DollarSign className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>
+            <span className='font-bold text-foreground'>{price}</span> / night
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Pin className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>{location.name}</span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Users className='w-4 h-4 text-primary' />
+          <span className='text-muted-foreground'>{maxGuests} Guests</span>
+        </div>
       </CardContent>
     </Card>
   );
