a6a7c6011a56328a82b0b5a8836552f835d235ca
SoundtrkSamurai <furtimat3@yahoo.com>
2025-04-16 22:40:54 -0700

    app-state: wire up filters with homepage

--- src/components/ListingList.jsx
+++ src/components/ListingList.jsx
@@ -1,10 +1,12 @@
+import { map } from 'lodash';
+
 import ListingCard from '@/components/ListingCard';
 
 const ListingList = ({ listings }) => {
   return (
     <div className='flex flex-wrap justify-center gap-4'>
       {listings.length > 0 ? (
-        listings.map((listing) => (
+        map(listings, (listing) => (
           <ListingCard key={listing.id} listing={listing} />
         ))
       ) : (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
+import { filter, includes, toLower } from 'lodash';
 import { useState } from 'react';
 
-import { listings as staticListings } from '@/api/data/listings';
+import {
+  isListingAvailable,
+  listings as staticListings,
+} from '@/api/data/listings';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator } from '@/components/ui';
@@ -8,13 +12,40 @@ import { Separator } from '@/components/ui';
 const HomePage = () => {
   const [listings, setListings] = useState(staticListings);
 
+  const handleFilters = (filters) => {
+    const { dates, guests, search } = filters;
+
+    let filteredListings = staticListings;
+
+    if (dates) {
+      filteredListings = filter(filteredListings, (listing) =>
+        isListingAvailable(listing, dates),
+      );
+    }
+
+    if (guests) {
+      filteredListings = filter(
+        filteredListings,
+        (listing) => guests < listing.maxGuests,
+      );
+    }
+
+    if (search) {
+      filteredListings = filter(filteredListings, (listing) =>
+        includes(toLower(listing.name), toLower(search)),
+      );
+    }
+
+    setListings(filteredListings);
+  };
+
   return (
     <div className='container py-4'>
       <div className='mb-4'>
-        <ListingFilters />
+        <ListingFilters onChange={handleFilters} />
         <Separator className='my-4' />
       </div>
-      <ListingList listings={staticListings} />
+      <ListingList listings={listings} />
     </div>
   );
 };
