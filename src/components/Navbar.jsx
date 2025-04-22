25ea9c62a915184606f32afabba9ab70733cdb7b
SoundtrkSamurai <furtimat3@yahoo.com>
2025-04-21 22:34:38 -0700

    st-mgmt: wire up new navbar with a Favorites listing page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Separator } from '@/components/ui';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='flex flex-row justify-center gap-8 px-8 py-4'>
+        <Link to='/'>Home</Link>
+        <Link to='/favorites'>Favorites</Link>
+      </div>
+      <Separator className='py-4' />
+    </>
+  );
+};
+
+export default Navbar;
