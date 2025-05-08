8ab48aa027d23de446bfcd82991ca9c6dd661462
SoundtrkSamurai <furtimat3@yahoo.com>
2025-05-08 16:32:08 -0700

    forms-auth: create route protection component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import NotFoundPage from '@/components/NotFoundPage';
+import Route from '@/components/Route';
 import HomePage from '@/pages/HomePage';
 import ListingDetailsPage from '@/pages/ListingDetailsPage';
 import ListingFavoritesPage from '@/pages/ListingFavoritesPage';
@@ -14,15 +15,23 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <NotFoundPage />,
     children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/favorites', element: <ListingFavoritesPage /> },
+      { path: '/', element: <Route isProtected={true}>
+        <HomePage />
+      </Route> },
+      { path: '/favorites', element: <Route isProtected={true}>
+        <ListingFavoritesPage />
+      </Route> },
       {
         path: '/listings/:listingId',
-        element: <ListingDetailsPage />,
+        element: <Route isProtected={true}>
+          <ListingDetailsPage />
+        </Route>,
       },
       {
         path: '/signin',
-        element: <SignInPage />,
+        element: (<Route>
+          <SignInPage />
+        </Route>),
       },
     ],
   },
