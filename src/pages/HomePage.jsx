578ad33ddb6e36e69296571600551c606d13ed6f
SoundtrkSamurai <furtimat3@yahoo.com>
2025-05-08 20:52:29 -0700

    extended: create custom hook to get details of specific listing ID

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useMemo, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import api from '@/api';
 import DataRenderer from '@/components/DataRenderer';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator } from '@/components/ui';
-import { fetchListings } from '@/state/listings/listingsSlice';
+import useListingsQuery from '@/hooks/queries/useListingsQuery';
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -24,10 +22,7 @@ const HomePage = () => {
     data: { data: listings } = {},
     isError,
     isLoading,
-  } = useQuery({
-    queryKey: ['listings', fetchOptions],
-    queryFn: () => api.get('/api/listings', fetchOptions),
-  });
+  } = useListingsQuery(fetchOptions);
 
   const handleFilters = useCallback((filters) => {
     setFilters(filters);
